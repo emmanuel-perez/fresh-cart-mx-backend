15c2702ed3d3a77b1f16197f83e89fa9076388df
Emmanuel <emmanuelalba115@gmail.com>
2023-12-08 12:13:44 -0600

    small change: Renamed productCategoriesRoutes router

--- src/routes/products-categories.routes.ts
+++ src/routes/products-categories.routes.ts
@@ -3,27 +3,25 @@ import { createProductCategory, deleteProductCategory, getAllProductCategories, 
 import { check } from "express-validator";
 import { validateDocumentIdExists, validateDocumentStatus, validateDocumentUniqueName, validateFields } from "../middlewares";
 import { ProductCategoryModel } from "../models";
-// import { validateFields, validateProductCategoryExists, validateProductCategoryStatus, validateProductCategoryUniqueName } from "../middlewares";
 
+export const productCategoriesRoutes = Router();
 
-export const productCategories = Router();
-
-productCategories.get('/', getAllProductCategories );
-productCategories.get('/:id', [
+productCategoriesRoutes.get('/', getAllProductCategories );
+productCategoriesRoutes.get('/:id', [
     check('id').custom(( id: string ) => validateDocumentIdExists( id, ProductCategoryModel ) ),
     check('id').custom(( id: string ) => validateDocumentStatus( id, ProductCategoryModel ) ),
     validateFields,
 ],getProductCategoryById );
-productCategories.post('/', [
+productCategoriesRoutes.post('/', [
     check('name').custom(( name: string ) => validateDocumentUniqueName( name, ProductCategoryModel ) ),
     validateFields,
 ],createProductCategory );
-productCategories.put('/:id', [
+productCategoriesRoutes.put('/:id', [
     check('id').custom(( id: string ) => validateDocumentIdExists( id, ProductCategoryModel ) ),
     check('id').custom(( id: string ) => validateDocumentStatus( id, ProductCategoryModel ) ),
     validateFields,
 ],updateProductCategory );
-productCategories.delete('/:id', [
+productCategoriesRoutes.delete('/:id', [
     check('id').custom((id: string) => validateDocumentIdExists( id, ProductCategoryModel ) ),
     check('id').custom(( id: string ) => validateDocumentStatus( id, ProductCategoryModel ) ),
     validateFields,
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { productCategories, productsRoutes } from './';
+import { productCategoriesRoutes, productsRoutes } from './';
 
 export const routes = Router();
 
 routes.use('/api/products', productsRoutes );
-routes.use('/api/product-categories', productCategories)
+routes.use('/api/product-categories', productCategoriesRoutes)
 
 
