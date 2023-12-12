7bdc768fbf9153c5949169de33cfd3b44fae0b29
Emmanuel <emmanuelalba115@gmail.com>
2023-12-12 15:58:09 -0600

    Fixed small bug with routes imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
-export * from './routes';
 
-//  ROUTES
 export * from './products-categories.routes';
 export * from './products.routes';
 export * from './users.routes';
+export * from './roles.routes';
+
+export * from './routes';
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { productCategoriesRoutes, productsRoutes } from './';
-import { rolesRoutes } from "./roles.routes";
+import { productCategoriesRoutes, productsRoutes, rolesRoutes, usersRoutes } from './';
 
 export const routes = Router();
 
 routes.use('/api/products', productsRoutes );
 routes.use('/api/product-categories', productCategoriesRoutes);
 routes.use('/api/roles', rolesRoutes );
-routes.use('/api/users', /*usersRoutes*/ );
+routes.use('/api/users', usersRoutes );
 
