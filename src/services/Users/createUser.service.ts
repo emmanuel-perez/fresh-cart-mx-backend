0e1e752268f6c23f7c7d362da83001935ce34406
Emmanuel <emmanuelalba115@gmail.com>
2023-12-13 17:28:49 -0600

    user password encryption: brcrypt

--- src/services/Users/createUser.service.ts
+++ src/services/Users/createUser.service.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from "express";
 import { UserModel } from "../../models";
+import { hashSync } from 'bcrypt';
 
 export const createUserService = async (req: Request, res: Response) => {
     try {
-        const { id, _id, ...body } = req.body;
+        const { id, _id, password, ...body } = req.body;
         const newUser = await UserModel.create({ ...body });
+        newUser.password = hashSync( password, 10 );
         return res.json( newUser );
     } catch (error) {
         return res.status(500).json({
