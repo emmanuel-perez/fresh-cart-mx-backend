41b707fc5c770ce44dd190af078d49d5b5f48c34
Emmanuel <emmanuelalba115@gmail.com>
2024-01-01 01:11:40 -0600

    getAllProducts: limit number of products in request and start parameter

--- src/services/Products/getAllProducts.service.ts
+++ src/services/Products/getAllProducts.service.ts
@@ -2,8 +2,23 @@ import { Request, Response } from "express";
 import { ProductModel } from "../../models";
 
 export const getAllProductsService = async ( req: Request, res: Response ) => {
-    const products = await ProductModel.find({ status: true })
-    return res.json( products );
+    const { limit=Number.POSITIVE_INFINITY, start=0 } = req.query;
+    const totalProductsPromise = ProductModel.countDocuments({ status: true });
+    const productsPromise = ProductModel.find({ status: true })
+        .limit( +limit )
+        .skip( +start );
+    try {
+        const [ totalProducts, products ] = await Promise.all([ totalProductsPromise, productsPromise ])
+        return res.json({
+            total_products: totalProducts, 
+            products,
+        });
+    } catch ( error ) {
+        return res.status(500).json({
+            msg: 'Could not get products',
+            error,
+        })
+    }
 }
 
 
