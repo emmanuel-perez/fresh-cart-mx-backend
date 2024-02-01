c3981df4519b9877183560a010802f102b78a631
Emmanuel <emmanuelalba115@gmail.com>
2024-02-01 12:18:46 -0600

    change delete product response format

--- src/services/ProductCategories/deleteProductCategory.service.ts
+++ src/services/ProductCategories/deleteProductCategory.service.ts
@@ -3,6 +3,9 @@ import { ProductCategoryModel } from "../../models";
 
 export const deleteProductCategoryService = async ( req: Request, res: Response ) => {
     const { id } = req.params;
-    const deletedProductCategory = await ProductCategoryModel.findByIdAndUpdate( id, { status: false } );
-    return res.json( deletedProductCategory );
+    const deletedProductCategory = await ProductCategoryModel.findByIdAndUpdate( id, { status: false }, { new: true } );
+    return res.json({
+        msg: 'product deleted succesfully',
+        deleted_product: deletedProductCategory
+    });
 }
